52c1103db1be087814be999d95e1316278a44c85
Welyelf R. Hisula <32445227+Welyelf@users.noreply.github.com>
2020-12-03 18:49:29 +0800

    Update google.php

--- application/config/google.php
+++ application/config/google.php
@@ -18,7 +18,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $config['client_id']        = '758165209849-mbmn536r244a2gk764udk4jshaoe5ag8.apps.googleusercontent.com';
 $config['client_secret']    = 'ZTNI2FBtBEJuPUbRU1J1YWJL';
-$config['redirect_uri']     = 'https://members.rewardsvine.com/auth/login/';
+$config['redirect_uri']     = 'https://members.rewardsvine.com/auth/login';
 $config['application_name'] = 'Rewards Vine';
 $config['api_key']          = 'AIzaSyCD6skS2RX-ccSTT95F5M3jhi6Arg8E1Jk';
 $config['scopes']           = array();
